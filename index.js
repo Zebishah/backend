20c8a6b72fc41b0c351cc768cb0da29805faa767
Zohaib haider <zebihaider123@gmail.com>
2023-10-20 02:39:26 +0500

    changes

--- index.js
+++ index.js
@@ -3,11 +3,18 @@ const express = require('express');
 const app = express();
 require('dotenv').config();
 var cors = require('cors')
+const allowedOrigins = ['https://front-end-jade-six.vercel.app'];
 app.use(cors({
-    origin: ["https://front-end-jade-six.vercel.app"],
-    methods: ["POST", "GET", "POST", "PUT"],
-    credentials: true
-}))
+    origin: (origin, callback) => {
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true, // if you are using cookies or sessions
+}));
 
 app.use(express.json())
 const port = 5000;
