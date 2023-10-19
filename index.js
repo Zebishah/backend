7cc421c61908a169035ccd3f6537f698a8d24492
Zohaib haider <zebihaider123@gmail.com>
2023-10-20 01:08:19 +0500

    changes

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ app.use(express.json())
 const port = 5000;
 connectDB();
 let host = process.env.REACT_APP_API_HOST
-app.use('/api/auth', require('./routes/auth'))
-app.use('/api/notes', require('./routes/notes'))
+app.use(`${host}/api/auth`, require('./routes/auth'))
+app.use(`${host}/api/notes`, require('./routes/notes'))
 app.listen(port, () => {
     console.log("Server is Listening at Port" + host);
 })
