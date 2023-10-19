b79235da8a5c0538f04bc27c854e795484f9be53
Zohaib haider <zebihaider123@gmail.com>
2023-10-20 01:39:15 +0500

    changes

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const app = express();
 require('dotenv').config();
 var cors = require('cors')
 app.use(cors({
-    origin: "https://front-end-jade-six.vercel.app",
+    origin: ["https://front-end-jade-six.vercel.app"],
+    methods: ["POST", "GET", "POST", "PUT"],
     credentials: true
 }))
 app.use(express.json())
