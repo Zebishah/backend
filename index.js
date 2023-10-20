5ebe6716ee156b0f75bcbdc711376ac8727cd5a4
Zohaib haider <zebihaider123@gmail.com>
2023-10-20 16:02:52 +0500

    changes

--- index.js
+++ index.js
@@ -4,13 +4,15 @@ const connectDB = require("./db");
 const express = require('express');
 const app = express();
 app.use(express.json())
+const cors = require('cors');
+
 app.use(cors({
     origin: 'https://front-end-jade-six.vercel.app',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     optionsSuccessStatus: 204, // No content response for preflight requests
 
 }));
-
+let host = process.env.HEROKU_BACKEND_URL;
 const port = 5000;
 connectDB();
 app.use(`/api/auth`, require('./routes/auth'))
