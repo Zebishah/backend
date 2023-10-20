73ab9eb68374552b5b62c5906a5b889a21188467
Zohaib haider <zebihaider123@gmail.com>
2023-10-20 12:47:49 +0500

    changes

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 app.use(express.json())
 app.use((req, res, next) => {
     // Set the CORS headers here to allow or deny cross-origin requests.
-    res.header('Access-Control-Allow-Origin', 'https://your-frontend-domain.com');
+    res.header('Access-Control-Allow-Origin', 'https://front-end-jade-six.vercel.app');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     res.header('Access-Control-Expose-Headers', 'Content-Length');
