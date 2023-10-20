dd50a8e4e22bc9c3e8ee69ab6952803a3d084ca9
Zohaib haider <zebihaider123@gmail.com>
2023-10-20 15:44:53 +0500

    changes

--- index.js
+++ index.js
@@ -1,35 +1,16 @@
-if (process.env.NODE_ENV !== 'production') {
-    require('dotenv').config({ path: __dirname + '/.env' });
-}
+
+require('dotenv').config();
 const connectDB = require("./db");
 const express = require('express');
-
 const app = express();
 app.use(express.json())
-app.use((req, res, next) => {
-    // Set the CORS headers here to allow or deny cross-origin requests.
-    res.header('Access-Control-Allow-Origin', 'https://front-end-jade-six.vercel.app');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    res.header('Access-Control-Expose-Headers', 'Content-Length');
-    res.header('Access-Control-Allow-Credentials', true);
+app.use(cors({
+    origin: 'https://front-end-jade-six.vercel.app',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    optionsSuccessStatus: 204, // No content response for preflight requests
 
-    // Call the 'next' function to pass the request to the next middleware or route handler.
-    next();
-});
+}));
 
-app.use(express.static(path.join(__dirname, "../front-end/build")));
-
-app.get("*", function (_, res) {
-    res.sendFile(
-        path.join(__dirname, "../frontend/build/index.html"),
-        function (err) {
-            if (err) {
-                res.status(500).send(err)
-            }
-        }
-    )
-})
 const port = 5000;
 connectDB();
 app.use(`/api/auth`, require('./routes/auth'))
